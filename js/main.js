5edd10ffc6430be1ed073491afb07dd2d656f0fa
Bartek Kruszczynski <bartek@kruszczynski.net>
2014-02-04 01:28:20 +0100

    get rid of the php

--- js/main.js
+++ js/main.js
@@ -1,43 +1,52 @@
-weatherUrl = "http://dogeweather.comoj.com/weather.php";
+(function(){
+    var locationAPI = 'http://ip-api.com/json';
+    var weatherAPI = 'http://api.openweathermap.org/data/2.5/weather?lang=pl&';
+    var getWeather = function(data) {
+        var status = data.status;
+        var fullWeatherEndpoint = weatherAPI;
+        if(status == 'success') {
+            fullWeatherEndpoint += 'lat=' + data.lat + '&lon=' + data.lon;
+        } else {
+            fullWeatherEndpoint += 'q=Paris';
+        }
+        $.getJSON(fullWeatherEndpoint,gotWeather)
+    }
+    var gotWeather = function(data) {
+        //set weather id & icon
+        var id = data.weather[0].id;
+        var icon = data.weather[0].icon;
 
-$.getJSON(weatherUrl, function (data) {
+        $('#weather-id').text(id);
+        $('#weather-icon').text(icon);
 
-    //$("#degreesCelsius").hide();
-//    console.log(data);
+        //TESTING
+        //icon = "01n";
+        //change such doge and sky based on much icon
+        var doge_img = "url(./img/doge/" + icon + ".png)";
+        $('.doge-image').css('background-image', doge_img);
 
-    //set weather id & icon
-    var id = data.weather[0].id;
-    var icon = data.weather[0].icon;
+        var sky_img = "url(./img/sky-img/" + icon + ".png)";
+        $('.bg').css('background-image', sky_img);
 
-    $('#weather-id').text(id);
-    $('#weather-icon').text(icon);
+    //    console.log(icon);
 
-    //TESTING
-    //icon = "01n";
-    //change such doge and sky based on much icon
-    var doge_img = "url(./img/doge/" + icon + ".png)";
-    $('.doge-image').css('background-image', doge_img);
+        //get weather description
+        var tempCelcius = data.main.temp - 273.15;
+        var tempFahrenheit = tempCelcius * 9 / 5 + 32;
+        var description = data.weather[0].description;
 
-    var sky_img = "url(./img/sky-img/" + icon + ".png)";
-    $('.bg').css('background-image', sky_img);
+        $('#weather-desc').text("wow " + description);
+        $('#location').text("uszanowanko " + data.name);
 
-//    console.log(icon);
+        $('#degreesCelsius .number').text(Math.round(tempCelcius));
+        $('#degreesCelsius .cel').text("°C ");
+        $('#degreesFahrenheit').text(Math.round(tempFahrenheit) + "°F");
 
-    //get weather description
-    var tempCelcius = data.main.temp - 273.15;
-    var tempFahrenheit = tempCelcius * 9 / 5 + 32;
-    var description = data.weather[0].description;
+        $(".suchlikes").show();
+        $(".ourinfo").show();
 
-    $('#weather-desc').text("wow " + description);
-    $('#location').text("uszanowanko " + data.name);
-
-    $('#degreesCelsius .number').text(Math.round(tempCelcius));
-    $('#degreesCelsius .cel').text("°C ");
-    $('#degreesFahrenheit').text(Math.round(tempFahrenheit) + "°F");
-
-    $(".suchlikes").show();
-    $(".ourinfo").show();
-
-    //initialise such doge
-    $($.doge);
-});
+        //initialise such doge
+        $($.doge);
+    };
+    $.getJSON(locationAPI,getWeather);
+}())
